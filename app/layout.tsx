c80b5b74abd0baac89a7fe34a5af9484c3b668c3
Arjun Nair <arjun@mac.lan>
2024-12-30 18:59:36 -0500

    vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/next';
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +33,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
